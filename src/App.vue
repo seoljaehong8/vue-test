49ba2601fa3945b3e8a01e4d79e873dc0aeac565
Ubuntu <ubuntu@ip-172-31-3-83.ap-northeast-2.compute.internal>
2021-07-06 16:39:39 +0000

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to JaeHong's Vue.js App"/>
+    <HelloWorld msg="Welcome to Jenkins Vue.js App"/>
   </div>
 </template>
 
